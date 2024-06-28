8b21aa639715f866c32194e5fbeeb282b1cfb662
bishop-of-intensity <79968439+bishop-of-intensity@users.noreply.github.com>
2024-06-28 18:53:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,10 +17,20 @@ $('.expander').click(function() {
     } else {
         $(this).parent().parent().removeClass('main-section-expanded');
         $(this).parent().parent().children('.main-content-container').removeClass('main-section-expanded');
+        $(this).parent().parent().children('.main-content-container').children('.main-content-container').removeClass('main-section-expanded');
     }
 });
 
 $('.expander-lvl-2').click(function() {
+    if(!$(this).parent().parent().hasClass('main-section-expanded')) {
+        $(this).parent().parent().addClass('main-section-expanded');
+    } else {
+        $(this).parent().parent().removeClass('main-section-expanded');
+        $(this).parent().parent().children('.main-content-container').removeClass('main-section-expanded');
+    }
+});
+
+$('.expander-lvl-3').click(function() {
     if(!$(this).parent().parent().hasClass('main-section-expanded')) {
         $(this).parent().parent().addClass('main-section-expanded');
     } else {
@@ -34,6 +44,8 @@ $('.close-all').click(function() {
     $(this).parent().children().children('.main-content-container').removeClass('main-section-expanded');
     $(this).parent().children().children('.main-content-container')
     .children('.main-content-container').removeClass('main-section-expanded');
+    $(this).parent().children().children('.main-content-container').children('.main-content-container')
+    .children('.main-content-container').removeClass('main-section-expanded');
 });
 
 $('.expand-all').click(function() {
@@ -42,6 +54,8 @@ $('.expand-all').click(function() {
     $(this).parent().children().children('.main-content-container').addClass('main-section-expanded');
     $(this).parent().children().children('.main-content-container')
     .children('.main-content-container').addClass('main-section-expanded');
+    $(this).parent().children().children('.main-content-container').children('.main-content-container')
+    .children('.main-content-container').addClass('main-section-expanded');
 });
 
 $('.expand-medium').click(function() {
@@ -49,5 +63,7 @@ $('.expand-medium').click(function() {
     $(this).parent().children().children('.main-content-container').addClass('main-section-expanded');
     $(this).parent().children().children('.main-content-container').removeClass('main-section-expanded');
     $(this).parent().children().children('.main-content-container')
+    .children('.main-content-container').removeClass('main-section-expanded');
+    $(this).parent().children().children('.main-content-container').children('.main-content-container')
     .children('.main-content-container').removeClass('main-section-expanded');
 });
